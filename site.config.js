20686f957b62fff057811e61eca441508da47380
Henri <HenriChabrand@users.noreply.github.com>
2021-06-06 09:51:50 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '27c114d739c84cfca5e35819b1248df9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
